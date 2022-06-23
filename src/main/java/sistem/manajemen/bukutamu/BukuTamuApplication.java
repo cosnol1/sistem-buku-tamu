c122b0b865c5849126c6be466681a0d792b279c3
khusnul.khotimah92 <khotimahkhusnul23@gmail.com>
2022-06-23 15:23:01 +0700

    create repo spring boot

---
+++ src/main/java/sistem/manajemen/bukutamu/BukuTamuApplication.java
@@ -0,0 +1,13 @@
+package sistem.manajemen.bukutamu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BukuTamuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BukuTamuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sistem/manajemen/bukutamu/BukuTamuApplicationTests.java
@@ -0,0 +1,13 @@
+package sistem.manajemen.bukutamu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BukuTamuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
